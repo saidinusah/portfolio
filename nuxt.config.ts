9d3bc6f33e72380713491bb962c7c48b4629bf63
Inusah Said <saidinusah29@gmail.com>
2023-09-03 14:05:07 +0000

    seo stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    pageTransition: { name: "page", mode: "out-in" },
-
     head: {
-      meta: [{}],
       link: [
         { rel: "preconnect", href: "https://fonts.googleapis.com" },
         {
@@ -17,6 +14,55 @@ export default defineNuxtConfig({
           href: 'https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,300;0,400;0,500;1,600&display=swap"',
         },
       ],
+      title: "Inusah Said: Software Developer (Frontend Web Developer)",
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1, shrink-to-fit=no",
+      meta: [
+        {
+          name: "description",
+          content: "Inusah Said's portfolio",
+        },
+        {
+          name: "keywords",
+          content: "Web Developer, Tech, Frontend Developer, Ghana",
+        },
+        {
+          name: "twitter:card",
+          content: "summary_large_image",
+        },
+        {
+          name: "theme-color",
+          content: "#111111",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          name: "og:title",
+          content: "Inusah Said: Software Developer (Frontend Web Developer)",
+        },
+        {
+          name: "og:description",
+          content:
+            "Inusah Said: Software Developer (Frontend Web Developer) based in Ghana",
+        },
+        {
+          property: "og:image",
+          content: "/public/android-chrome-512x512.png",
+        },
+        {
+          property: "og:type",
+          content: "website",
+        },
+        {
+          property: "og:url",
+          content: "https://saidinusah.vercel.app/",
+        },
+      ],
+      htmlAttrs: {
+        lang: "en",
+      },
     },
   },
 
